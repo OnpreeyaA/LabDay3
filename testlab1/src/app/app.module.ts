33e9681bdf9f6381abdc48297ed0c5682e860110
AoiJai <aoi.onpreeya@gail.com>
2019-02-17 11:01:59 +0700

    Cute Aoi

--- testlab1/src/app/app.module.ts
+++ testlab1/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { FormComponent } from './form/form.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    FormComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
